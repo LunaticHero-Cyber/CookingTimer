59b9dcf4222ec14f7981cbd6463f1986b66a96e8
Fred_Michael <fred.konsultan@work.bri.co.id>
2025-02-20 08:38:30 +0700

    feat: set up folder path

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          '@/components': './src/components',
+          '@/views': './src/views',
+          '@/config': './src/config',
+          '@/native-modules': './src/native-modules',
+          '@/hooks': './src/hooks',
+          '@/routes': './src/routes',
+          '@/store': './src/store',
+          '@/types': './src/types',
+          '@/utils': './src/utils',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
